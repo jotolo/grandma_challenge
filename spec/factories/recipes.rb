d8b6c6fc8d02b702ea6957ad38139c44a69e0dab
Jorge T <jotolo7@gmail.com>
2018-08-10 15:56:00 -0400

    Adding seeds to populate the DB Adding Dockerfile Adding docker-compose.yml Changing the database configuration file to add new environment variables

--- spec/factories/recipes.rb
+++ spec/factories/recipes.rb
@@ -2,5 +2,8 @@ FactoryBot.define do
   factory :recipe do
     name { Faker::Food.dish }
     recipe { Faker::Food.description }
+    ingredient_ids {
+      FactoryBot.create_list(:ingredient, rand(10)).pluck(:id)
+    }
   end
 end
